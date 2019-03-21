b7fc481bd9b4bae68ab3a51b760f457326652761
Trevor Troxel <ttroxel21@my.whitworth.edu>
2019-03-20 21:28:57 -0700

    Add files via upload

--- Translator/Translator/Translator.cpp
+++ Translator/Translator/Translator.cpp
@@ -1,66 +1,174 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
 #include <iostream>
 #include <string>
-#include <sstream>
 using namespace std;
-//What this is is babies first coding is what this really is. 
-//This is still a massive work in progress, so there is not much right now, but more will progress will be made. So far only one and two work.
+
+class Language
+{
+public:
+	virtual void trans_say(string msg) = 0;
+	virtual void trans_if(string expr, string bool_expr) = 0;
+	virtual void trans_while(string expr, string bool_expr) = 0;
+
+	void getStatements() {
+		bool quit = false;
+		while (!quit)
+		{
+			cout << "What command do you want?\n";
+			string cmd, sub;
+			cin >> cmd;
+			getline(cin, sub);
+
+			int i;
+			string newSen = "";
+			for (i = 1; i < sub.length(); i++)
+			{
+				newSen += sub.at(i);
+			}
+
+			if (cmd == "say:")
+			{
+				trans_say(newSen);
+			}
+
+			else if (cmd == "if:")
+			{
+				cout << "What is the boolean expression?\n";
+				string bool_expr;
+				getline(cin, bool_expr);
+
+				trans_if(newSen, bool_expr);
+			}
+
+			else if (cmd == "while:")
+			{
+				cout << "What is the boolean expression?\n";
+				string bool_expr;
+				getline(cin, bool_expr);
+
+				trans_while(newSen, bool_expr);
+			}
+
+			else
+			{
+				cout << "Here is a list of commands of possible commands, depending on the language:" << endl;
+				cout << "say:\n" <<
+					"if:\n" <<
+					"elseif:\n" <<
+					"else:\n" <<
+					"for:\n" <<
+					"while:\n" <<
+					"value:\n" <<
+					"bigValue:\n" <<
+					"pointValue:\n" <<
+					"sentence:\n" <<
+					"letter:\n\n" <<
+
+					//Not what I want for the final product, but an ok sub for the time. Come back to this because this is like putting a band-aid over a broken bone
+					//"If you want to use any of these, just enter the name." << "Then you will get a promt to enter the two variables that you want and also calculate it for you.\n" <<
+					"equal:\n" <<
+					"equivalent:\n" <<
+					"notEquivalent:\n" <<
+					"module:\n" <<
+					"subtract:\n" <<
+					"add:\n" <<
+					"multiply:\n" <<
+					"divide:\n" << endl;
+			}
+		}
+	}
+};
+
+class CPP : public Language
+{
+public:
+	virtual void trans_say(string msg)
+	{
+		cout << "cout << \"" << msg << "\" << endl;\n";
+	}
+
+	virtual void trans_if(string expr, string bool_expr)
+	{
+		cout << "if (" << expr << ")\n" << "{\n" << bool_expr << endl << "}\n";
+		getStatements();
+	}
+
+	virtual void trans_while(string expr, string bool_expr)
+	{
+		cout << "while (" << expr << ")\n" << "{\n" << bool_expr << endl << "}\n";
+		getStatements();
+	}
+};
+
+class Java : public Language
+{
+public:
+	virtual void trans_say(string msg)
+	{
+		cout << "System.out.print(\"" << msg << "\")\n;";
+	}
+
+	virtual void trans_if(string expr, string bool_expr)
+	{
+		cout << "if (" << expr << ")\n" <<  "{\n" << bool_expr << endl << "}\n";
+		getStatements();
+	}
+
+	virtual void trans_while(string expr, string bool_expr)
+	{
+		cout << "while (" << expr << ")\n" << "{\n" << bool_expr << endl << "}\n";
+		getStatements();
+	}
+};
+
+/*class Python : public Language
+{
+public:
+	virtual void trans_say(string msg)
+	{
+		cout << "print(\"" << msg << "\");\n";
+	}
+
+	virtual void trans_if(string msg)
+	{
+
+	}
+};*/
+
+void start()
+{
+	cout << "Choose C++ or Java: ";
+	int i;
+	string lang = "";
+
+	cin >> lang;
+	cin.ignore();
+
+
+	Language *l = NULL;
+	if (lang == "C++")
+	{
+		l = new CPP();
+	}
+	else if (lang == "Java")
+	{
+		l = new Java();
+	}
+
+	else
+	{
+		cout << "Try again numb nuts\n";
+		start();
+	}
+	
+	l->getStatements();
+}
+
 int main()
 {
-	cout << "This will be a pseudo code translator. WIP" << endl;
-	cout << "It takes what you put in and translate it into C++. Hopefully other languages too" << endl;
-	cout << "EX: say: hello world, this will translate to cout << hello world << endl;" << endl;
-	cout << "First say what you would like to say. If you want to pick a statement, type the name of the number you want. EX: if you want a cout statement, type one" << endl;
-	cout << "The list goes:" << endl;
-	cout << "1. say: = cout << (your sentence) << endl;"<< endl << "2. if: = if()" << endl << "3. else if: = else if ()" << endl << "4. else: = else()" << endl << "5. for: = for ()" << endl << "6. while: = while ()" <<
-	endl << "7. value: = int" << endl << "8. bigValue: = double" << endl <<	"9. pointValue: float" << endl << "10. sentence: = string" << endl << "11. letter: = char" <<
-	endl << "12. module: = %" << endl << "13. equal: = =" << endl << "14. equivalent: = ==" << endl << "15. subtract: = -" << endl << "16. add: = +" << endl << "17. multiply: = *"  <<
-	endl << "18. divide: = /" << endl << "19. notEquivalent: = !=" << endl << endl;
-
-	string x;
-	getline(cin, x);
-
-	if(x == "one") //the say: input
-	{
-		cout << "Next say what you want to put into the sentence. EX: hello world" << endl;
-
-		string sentence;
-		getline(cin, sentence);
-
-		cout << "cout << " << sentence << " << endl;" << endl;
-	}
-	else if(x == "two") //if statement
-	{
-		cout << "What do you want to say? EX: x is equal 1" << endl;
-
-		string statement;
-		getline(cin, statement);
-		
-		cout << "Now say what what would happin if that first statement was true" << endl;
-
-		string outcome;
-		getline(cin, outcome);
-			
-		cout << "if(" << statement << ")" << endl;
-		cout << "{" << endl;
-		cout << outcome << endl;
-		cout << "}" << endl;
-
-	}
-	
-/*string input;
-	getline(cin, input);
-
-	cout << "next say what you want to put into the sentence. EX: hello world"<< endl;
-
-	string sentence;
-	getline(cin, sentence);
-	
-	if (input == "say:")
-	{
-		cout << "cout << " << sentence << " << endl;" << endl;
-	}*/
-
-
-system("pause");
+	start();
+	return 0;
 }
 
